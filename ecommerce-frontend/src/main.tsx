ef9074071afe57f0b610227c9b8b09f6ca64d61c
Herik-Thiury <herikthiury2017@gmail.com>
2025-10-30 20:04:50 -0300

    front

---
+++ ecommerce-frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+// 1. Importa o CSS do Bootstrap no ponto de entrada
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Remove o import do index.css, pois usaremos o Bootstrap
+// import './index.css'; 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
